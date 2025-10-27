f2b15d9c33fd59338929aee039248d2dfd5661b3
Le Duc An <leducan.ttt@gmail.com>
2025-10-27 22:36:54 +0700

    Changes

---
+++ Backend/src/models/rooms.js
@@ -0,0 +1,18 @@
+import db from '../config/db.js'
+
+export async function initRoomsModel() {
+    const createRoomTable = `
+        create table IF NOT EXISTS rooms (
+            room_id int auto_increment primary key,
+            room_number VARCHAR(10) NOT NULL UNIQUE,
+            room_type varchar(255) NOT NULL,
+            price DECIMAL(10,2) NOT NULL,
+            status ENUM('available', 'booked', 'maintenance') DEFAULT 'available',
+            description TEXT,
+            image_url VARCHAR(255)  -- store file path or URL
+    )
+    `;
+
+    await db.query(createRoomTable);
+    console.log('Room table created.')
+}
