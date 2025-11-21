758f77cce1496bdfaa4373d289202c4b791de28b
Le Duc An <leducan.ttt@gmail.com>
2025-11-21 21:48:56 +0700

    Updated

--- Frontend/src/component/RoomFilter.tsx
+++ Frontend/src/component/RoomFilter.tsx
@@ -4,9 +4,11 @@ import { Label } from "../ui/label";
 import { Slider } from "../ui/slider";
 import { Checkbox } from "../ui/checkbox";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
+import { Input } from "../ui/input";
 import type { Room } from "../pages/Room.tsx";
 import { Button } from "../ui/button.tsx";
 import { RotateCcw } from "lucide-react";
+import { getAvailableRooms } from '../utils/APIFunction.ts'
 
 interface RoomFiltersProps {
   rooms: Room[];
@@ -18,6 +20,8 @@ export function RoomFilters({ rooms, onFilterChange }: RoomFiltersProps) {
   const [selectedTypes, setSelectedTypes] = useState<string[]>([]);
   const [selectedStatuses, setSelectedStatuses] = useState<string[]>([]);
   const [selectedCapacity, setSelectedCapacity] = useState<string>("all");
+  const [selectedCheckInDate, setSelectedCheckInDate] = useState<string>("");
+  const [selectedCheckOutDate, setSelectedCheckOutDate] = useState<string>("");
 
   const roomTypes = Array.from(new Set(rooms.map((r) => r.standard)));
   const statuses = [
@@ -27,33 +31,48 @@ export function RoomFilters({ rooms, onFilterChange }: RoomFiltersProps) {
   ];
 
   useEffect(() => {
-    let filtered = rooms;
+    const applyFiltered = async() => {
+      let filtered = rooms;
 
-    // Filter by price
-    filtered = filtered.filter(
-      (room) => room.price >= priceRange[0] && room.price <= priceRange[1]
-    );
-
-    // Filter by type
-    if (selectedTypes.length > 0) {
-      filtered = filtered.filter((room) => selectedTypes.includes(room.standard));
-    }
-
-    // Filter by status
-    if (selectedStatuses.length > 0) {
-      filtered = filtered.filter((room) =>
-        selectedStatuses.includes(room.status)
+      // Filter by price
+      filtered = filtered.filter(
+        (room) => room.price >= priceRange[0] && room.price <= priceRange[1]
       );
-    }
 
-    // Filter by capacity
-    if (selectedCapacity !== "all") {
-      const capacity = parseInt(selectedCapacity);
-      filtered = filtered.filter((room) => room.capacity >= capacity);
-    }
+      // Filter by type
+      if (selectedTypes.length > 0) {
+        filtered = filtered.filter((room) => selectedTypes.includes(room.standard));
+      }
 
-    onFilterChange(filtered);
-  }, [priceRange, selectedTypes, selectedStatuses, selectedCapacity, rooms, onFilterChange]);
+      // Filter by status
+      if (selectedStatuses.length > 0) {
+        filtered = filtered.filter((room) =>
+          selectedStatuses.includes(room.status)
+        );
+      }
+
+      // Filter by capacity
+      if (selectedCapacity !== "all") {
+        const capacity = parseInt(selectedCapacity);
+        filtered = filtered.filter((room) => room.capacity >= capacity);
+      }
+
+      let guests = 0;
+      if (selectedCapacity !== 'all') {
+        guests = parseInt(selectedCapacity);
+      }
+      if (selectedCheckInDate !== '' && selectedCheckOutDate !== '') {
+        const res = await getAvailableRooms(selectedCheckInDate, selectedCheckOutDate, guests);
+        const result = res.data;
+        const IDs = new Set(result.map((room: any) => room.room_id));
+        filtered = filtered.filter(room => IDs.has(room.room_id));
+      }
+      onFilterChange(filtered);
+      console.log(filtered);
+    }
+    
+    applyFiltered();
+  }, [priceRange, selectedTypes, selectedStatuses, selectedCapacity, rooms, onFilterChange, selectedCheckInDate, selectedCheckOutDate]);
 
   const handleTypeToggle = (type: string) => {
     setSelectedTypes((prev) =>
@@ -76,6 +95,8 @@ export function RoomFilters({ rooms, onFilterChange }: RoomFiltersProps) {
     setSelectedTypes([]);
     setSelectedStatuses([]);
     setSelectedCapacity("all");
+    setSelectedCheckInDate('');
+    setSelectedCheckOutDate('');
   };
 
   return (
@@ -168,6 +189,19 @@ export function RoomFilters({ rooms, onFilterChange }: RoomFiltersProps) {
               <SelectItem value="6">6+ people</SelectItem>
             </SelectContent>
           </Select>
+        </div>
+
+        {/* Date */}
+        <div className="space-y-3">
+          <Label htmlFor="date">Date</Label>
+          <div className="flex space-x-2">
+            <span className="text-sm text-gray-500 self-center">From</span>
+            <Input value={selectedCheckInDate} type="date" id="date" className="w-full" placeholder="Check-in Date" onChange={(e) => setSelectedCheckInDate(e.target.value)}/>
+          </div>
+          <div className="flex space-x-2">
+            <span className="text-sm text-gray-500 self-center">To</span>
+            <Input value={selectedCheckOutDate} type="date" id="date" className="w-full" placeholder="Check-out Date" onChange={(e) => setSelectedCheckOutDate(e.target.value)}/>
+          </div>
         </div>
       </CardContent>
     </Card>
