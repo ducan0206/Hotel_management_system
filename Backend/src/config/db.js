e6b952f8b491ade4b330feefdb36fc7f53658621
Le Duc An <leducan.ttt@gmail.com>
2025-12-27 00:47:48 +0700

    Fix: cloud config

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -7,6 +7,7 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    connectionLimit: 4,
     waitForConnections: true,
     connectionLimit: 3,
 })
