b9ce274ea812d6eb95db0b9581b61c6a3ed3393a
Le Duc An <leducan.ttt@gmail.com>
2025-12-26 19:37:31 +0700

    Fix

--- Backend/src/helper/customer.js
+++ Backend/src/helper/customer.js
@@ -4,7 +4,7 @@ export const fetchAllCustomers = async() => {
     try {
         const [customers] = await db.query(
             `
-            select c.KH_id, a.full_name, a.phone, a.email, a.username, a.created_at, count(b.booking_id) as total_booking, coalesce((b.total_price), 0) as total_spent
+            select a.user_id, c.KH_id, a.full_name, a.phone, a.id_card, a.date_of_birth, a.gender, a.address, a.email, a.username, a.created_at, count(b.booking_id) as total_booking, coalesce((b.total_price), 0) as total_spent
             from Account a join Customers c on a.user_id = c.account_id
                            left join Bookings b on a.user_id = b.user_id
             where a.role = 'customer'
@@ -48,26 +48,35 @@ export const getInfo = async(id) => {
 // how can fix with general cases: changes personal info, password, ...
 export const updatingCustomerInfo = async(id, infoData) => {
     try {
+        console.log(id);
+        console.log(infoData);
         const [existingCustomer] = await db.query(
             `
             select user_id, full_name, phone, email, created_at from Account 
             where user_id = ? and role = 'customer'
-            `
+            `, [id]
         )
         if(existingCustomer.length === 0) {
             throw new Error(`Customer with id ${id} not found.`);
         }
-        const {fullname, phone, email} = infoData;
+        const {full_name, phone, email, address, date_of_birth, gender, id_card} = infoData;
         await db.query(
             `
-            update from Account
+            update Account
                 set full_name = ?,
-                set phone = ?,
-                set email = ?
+                    phone = ?,
+                    email = ?,
+                    address = ?,
+                    date_of_birth = ?,
+                    gender = ?,
+                    id_card = ?
             where user_id = ?
-            `, [fullname, phone, email, id]
+            `, [full_name, phone, email, address, date_of_birth, gender, id_card, id]
         )
-        return {message: `The information of customer with id ${id} has been updated.`}
+        return {
+            status: 200,
+            message: `The information of customer with id ${id} has been updated.`
+        }
     } catch (error) {
         console.log('Error: updatingCustomerInfo function', error.message);
         return error;
